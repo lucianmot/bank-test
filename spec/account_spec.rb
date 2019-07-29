5285695df5e77458bf8f093220c2437e98279ef2
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-29 13:49:00 +0100

    created account with balance attached as a properly of the object

---
+++ spec/account_spec.rb
@@ -0,0 +1,17 @@
+require 'account'
+
+describe Account do
+  it "when you initialize an account you should have a balance" do
+    account = Account.new
+    expect(account). to respond_to(:balance)
+  end
+  it "when you initialize an account the balance should be 0" do
+    account = Account.new
+    expect(account.balance).to eq(0)
+  end
+  xit "when you deposit cash into your account it should change your account balance accordingly" do
+    account = Account.new
+    account.deposit(500)
+    expect(account.balance). to eq(500)
+  end
+end
