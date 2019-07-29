a7cc2be31bdfec8b4e271e5ec4db12f73166f589
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-29 14:04:58 +0100

    dry-ed out the 3rd test to make it more pro

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,10 @@
 class Account
 attr_reader :balance
   def initialize
-  @balance = 0
+    @balance = 0
+  end
+
+  def deposit(sum)
+    @balance += sum
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,26 @@
 require 'account'
 
 describe Account do
+
   it "when you initialize an account you should have a balance" do
     account = Account.new
     expect(account). to respond_to(:balance)
   end
+
   it "when you initialize an account the balance should be 0" do
     account = Account.new
     expect(account.balance).to eq(0)
   end
+
+  it "when you deposit cash into your account it should change your account balance accordingly" do
+    account = Account.new
+    expect{account.deposit(500)}.to change{account.balance}.by(500)
+  end
+
   xit "when you deposit cash into your account it should change your account balance accordingly" do
     account = Account.new
-    account.deposit(500)
+
+    account.withdrawal(500)
     expect(account.balance). to eq(500)
   end
 end
