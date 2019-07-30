8289d8c3dce65a9d1d4129d3ea55eca23836f82c
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-30 15:19:09 +0100

    paused at deposit adding to history statement

--- lib/account.rb
+++ lib/account.rb
@@ -4,10 +4,13 @@ attr_reader :balance, :statement, :current_time
     @balance = 0
     @statement = { 1 => "date || credit || debit || balance" }
     @current_time = Time.now.strftime("%d/%m/%Y")
+    @id = 1
   end
 
   def deposit(sum)
-    @balance += sum
+    @id = @id + 1
+    @statement[@id] = @current_time + ' || ' + sum + ' || || ' + @balance
+    @balance = @balance + sum
   end
 
   def withdrawal(sum)
